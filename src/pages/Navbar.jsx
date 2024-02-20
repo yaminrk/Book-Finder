fd3f2d0e14deddbad9a72ce0e309b2ffa7beee65
Yamin Rashid khan <yaminrk081@gmail.com>
2024-02-20 01:30:10 +0000

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 bg-white shadow flex items-center h-16">
+        <div className=" mx-auto px-2 sm:px-4 lg:px-8 bg-white shadow flex items-center h-16">
             <h2 className='font-bold'>BOOK-FINDER</h2>
         </div>
       
